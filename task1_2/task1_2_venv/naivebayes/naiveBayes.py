658c8cf3b4612a9a346a35a186285c00cf88fdbc
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2017-04-12 18:43:34 +0900

    Implement naive Bayes

--- task1_2/task1_2_venv/naivebayes/naiveBayes.py
+++ task1_2/task1_2_venv/naivebayes/naiveBayes.py
@@ -4,50 +4,79 @@ import MeCab
 
 class naiveBayes():
     def __init__(self):
-        self.categories = set()   # categorie set
+        self.catcount = {}        # catcount[cat] appearance of categories
+        self.wordcount = {}       # wordcount[cat][word] appearance of words in categories
         self.vocabularies = set() # vocabulary set
-        self.wordcount = {}       # likelihood # wordcount[cat][word] appearance of words in categories
-        self.catcount = {}        # prior # catcount[cat] appearance of categories
+
+    def to_words(self, sentence):
+        mecab = MeCab.Tagger('mecabrc')
+        words = [] # store words identified
+        for info in mecab.parse(sentence).split('\n'):
+            if info == 'EOS' or info == '': # exclude EOS and ''
+                continue
+            keyword = info.split(',')[0] # need only first one
+            aword = keyword.split('\t') # separate word and class
+            if aword[1] == '名詞': # if noun
+                words.append(aword[0])
+            elif aword[1] == '動詞': # if verb
+                words.append(aword[0])
+            elif aword[1] == '形容詞': # if adverb
+                words.append(aword[0])
+            elif aword[1] == '感動詞': # if ...
+                words.append(aword[0])
+        return words
+
+    def count_word(self, category, words):
+        for word in words:
+            self.wordcount.setdefault(category,{})
+            self.wordcount[category].setdefault(word,0)
+            self.wordcount[category][word] += 1
+            self.vocabularies.add(word)
+
+    def count_category(self, category):
+        self.catcount.setdefault(category,0)
+        self.catcount[category] += 1
 
     def train(self, data):
-        # train naive bayes
-        # initialize
-        for d in data:
-            cat = d[0]
-            self.categories.add(cat)
-        for cat in self.categories:
-            self.wordcount[cat] = defaultdict(int)
-            self.catcount[cat] = 0
-        # count categories and words
-        for d in data:
-            cat, doc = d[0], d[1:]
-            self.catcount[cat] += 1
-            for word in doc:
-                self.vocabularies.add(word)
-                self.wordcount[cat][word] += 1
+        category = data[0]
+        self.count_category(category)
+        words = self.to_words(data[1]):
+        self.count_word(category, words)
 
-    def classify(self, doc):
-        best = None
-        max = -sys.maxint
-        for cat in self.catcount.keys():
-            p = self.score(doc, cat)
-            if p > max:
-                max = p
-                best = cat
-        return best
+    def prior_prob(self, category):
+        num_of_category = sum(self.catcount.values())
+        num_of_data_of_category = self.catcount[category]
+        return num_of_docs_of_category / num_of_category
 
-    def wordProb(self, word, cat):
-        # calculate P(word|cat)
-        return float(self.wordcount[cat][word]+1) / float(self.denominator[cat])
+    def is_word_in_category(self, category, word):
+        if word in self.wordcount[category]:
+            return self.wordcount[category][word]
+        return 0
 
-    def score(self.catcount.values()):
-        total = sum(self.catcount.values())
-        score = math.log(float(self.catcount[cat]) / total)
-        for word in doc:
-            score += math.log(self.wordProb(word, cat))
+    def word_prob_given_category(self, category, word):
+        numer = self.is_word_in_category(category, word) + 1
+        denom = sum(self.wordcount[category].values()) + len(self.vocabularies)
+        prob = numer / denom
+        return prob
+
+    def score(self, category, words):
+        score = math.log(self.prior_prob(category))
+        for word in words:
+            score += math.log(self.word_prob_given_category(category, word))
         return score
+
+    def classify(self, words):
+        best = None
+        max = -sys.maxsize
+        for category in self.catcount.keys():
+            prob = self.score(category, words)
+            if prob > max:
+                max = prob
+                best = category
+        return best
 
     def __str__(self):
         total = sum(self.catcount.values())
-        return "%d" % len(self.categories)
-
+        categories = list(self.catcount.keys())
+        strng = "documents %d, vocabularies %d, categories %d" % (total, len(self.vocabularies), len(categories))
+        return strng
