84ef61bfecc912bd874f3719c80075c21730b78b
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2017-04-17 13:39:05 +0900

    Add new training data and test data

--- task1_2/task1_2_venv/polls/naivebayes/predict.nb.py
+++ task1_2/task1_2_venv/polls/naivebayes/predict.nb.py
@@ -20,11 +20,13 @@ test = args[2]
 valid = open(test, "r")
 catcount = {}
 correct = {}
+categories = {'1':'エンタメ','2': 'スポーツ','3':'おもしろ','4':'国内',
+              '5':'海外','6':'コラム','7':'IT・科学','8':'グルメ', }
 for temp in valid:
     data = temp.split(',')
-    true = data[0]
+    true = categories[data[0]]
     # init
-    correct.setdefault(true,0)
+    correct.setdefault(true, 0)
     catcount.setdefault(true, 0)
     catcount[true] += 1
     # inference
